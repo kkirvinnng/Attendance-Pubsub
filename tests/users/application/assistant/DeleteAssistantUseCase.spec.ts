14d8fbd1aaf779682fc9bce601f77e49119ce196
kkirvinnng <kevinreyna2480@gmail.com>
2022-11-06 22:30:33 -0300

    fix: assistant import naming

--- tests/users/application/assistant/DeleteAssistantUseCase.spec.ts
+++ tests/users/application/assistant/DeleteAssistantUseCase.spec.ts
@@ -1,6 +1,6 @@
 import 'reflect-metadata'
 import { AssistantNotFound } from '../../../../src/users/application/errors/AssistantNotFound'
-import { DeleteAssistantUseCase } from '../../../../src/users/application/use-cases/Assistant/DeleteAssistant.usecase'
+import { DeleteAssistantUseCase } from '../../../../src/users/application/use-cases/assistant/DeleteAssistant.usecase'
 import { Assistant } from '../../../../src/users/domain/entities/Assistant'
 import { FirebaseAssistantRepositoryMock } from '../../__mocks__/FirebaseAssistantRepositoryMock'
 
