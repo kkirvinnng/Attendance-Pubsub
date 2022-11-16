c479598636b75b9c098a007b4f8041d7e0b1618d
kkirvinnng <kevinreyna2480@gmail.com>
2022-11-16 19:09:45 -0300

    feat: add https function

--- src/functions-handler/App.https.ts
+++ src/functions-handler/App.https.ts
@@ -1,6 +1,6 @@
 import * as functions from 'firebase-functions'
 import app from '../api'
 
-export const Api = functions
+export const api = functions
     .runWith({ memory: '256MB', timeoutSeconds: 300 })
     .https.onRequest(app)
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export * from './main'
-export * from './functions-handler/AppHttp'
+export * from './functions-handler/App.https'
 export * from './functions-handler/UserCreated.pubsub'
 export * from './functions-handler/TeacherDeleted.pubsub'
 export * from './functions-handler/CommissionAssigned.pubsub'
